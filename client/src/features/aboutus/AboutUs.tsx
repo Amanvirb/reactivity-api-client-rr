ed29751d0dbb6a1e2a705a5fa2e5363e6599fd38
rajpal <rajpalbains82@gmail.com>
2023-08-09 09:44:58 +0200

    aboutUsTest

--- client/src/features/aboutus/AboutUs.tsx
+++ client/src/features/aboutus/AboutUs.tsx
@@ -5,6 +5,8 @@ const AboutUs = () => {
   return (
     <Box component="div"  sx={{ m: 6 }}>
         This page is under Construction
+         This page is under Construction
+          This page is under Construction
     </Box>
   )
 }
